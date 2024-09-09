aacf0142a8f319d06a533fa42f18505a0a6614a1
roshnadhakal <146826039+roshnadhakal@users.noreply.github.com>
2024-09-09 14:16:31 +0545

    Delete numberguess.py

--- numberguess.py
+++
@@ -1,6 +0,0 @@
-gameNum = 70
-userNum = int(input("Guess the number: "))
-while gameNum != userNum :
-    userNum= int(input("You entered wrong number. Guess Again! : "))
-
-print("Congratulation! you entered correct number", gameNum)
