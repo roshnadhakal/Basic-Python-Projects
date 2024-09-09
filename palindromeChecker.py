7763654a8adce540a6c671e32e11557d0b319e89
roshnadhakal <146826039+roshnadhakal@users.noreply.github.com>
2024-09-09 14:26:17 +0545

    Delete palindromeChecker.py

--- palindromeChecker.py
+++
@@ -1,7 +0,0 @@
-word = input("Enter a word: ")
-reverse_word = word[::-1]
-
-if(reverse_word == word):
-    print("The word",word,"is a palindrome")
-else:
-    print("The word",word,"is not a palindrome")
